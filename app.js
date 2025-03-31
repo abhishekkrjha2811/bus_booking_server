e0076e9c955792287c2f099b6e2a6a382b74dea9
Abhishek kumar jha <Abhishekkrjha34@gmail.com>
2025-03-31 11:03:40 +0530

    Backend models are added

---
+++ app.js
@@ -0,0 +1,34 @@
+dotenv.config();
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/connect.js";
+import { PORT } from "./config/config.js";
+
+
+const app = express();
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Server started on http://localhost/${PORT}`);
+      }
+    });
+  } catch (error) {
+    console.log("Error starting a server -->", error);
+  }
+};
+
+start();
---
+++ models/ticket.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const TickerSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  bus: { type: Schema.Types.ObjectId, ref: "Bus", required: true },
+  date: { type: date, required: true },
+  seatNumber: [{ type: Number, required: true }],
+  total_fare: { type: Number, required: true },
+  status: {
+    type: String,
+    enum: ["Upcoming", "Completed", "Cancelled"],
+    default: "Upcoming",
+  },
+  bookedAt: { type: Date, default: Date.now },
+  pnr: { type: String, unique: true, required: true },
+});
+
+const Ticket = mongoose.model("Ticket", TickerSchema);
+export default Ticket;
